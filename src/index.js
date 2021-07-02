76e5dd1b3377fede4baad7d1843e1ee4c7958720
admin <admin@admins-MBP.fios-router.home>
2021-07-01 22:11:14 -0400

    your message

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots } from './robots';
+import App from './containers/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
-);
+); 
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
